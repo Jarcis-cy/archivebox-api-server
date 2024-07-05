229a5f218ba74ab53d0aea40233c1f553528d12b
Jarcis-cy <ppgiant7@gmail.com>
2024-07-05 17:31:58 +0800

    [add] 添加解析日志函数

--- api/tests.py
+++ api/tests.py
@@ -1,12 +1,12 @@
-import os
 import unittest
 from unittest.mock import patch, MagicMock
-import subprocess
+
 import requests
-from api.utils import check_docker_version, check_docker_compose, initialize_archivebox
+
+from api.utils import check_docker_version, check_docker_compose, initialize_archivebox, parse_log
 
 
-class TestUtils(unittest.TestCase):
+class TestInitArchiveBox(unittest.TestCase):
 
     @patch('api.utils.subprocess.check_output')
     def test_check_docker_version_sufficient(self, mock_check_output):
@@ -144,5 +144,278 @@ class TestUtils(unittest.TestCase):
         mock_get.assert_called_once()
         mock_run.assert_not_called()
 
-if __name__ == '__main__':
-    unittest.main()
+
+class FormatOutputTest(unittest.TestCase):
+
+    def setUp(self):
+        self.single_target_success = """
+[i] [2024-07-04 06:16:07] ArchiveBox v0.7.2: archivebox add --depth=0 https://www.baidu.com/ --extract title,screenshot
+    > /data
+
+[+] [2024-07-04 06:16:09] Adding 1 links to index (crawl depth=0)...
+    > Saved verbatim input to sources/1720073769-import.txt
+    > Parsed 1 URLs from input (Generic TXT)
+    > Found 1 new URLs not already in index
+
+[*] [2024-07-04 06:16:09] Writing 1 links to main index...
+    √ ./index.sqlite3
+
+[*] [2024-07-04 06:16:09] Archiving 1/5 URLs from added set...
+
+[▶] [2024-07-04 06:16:09] Starting archiving of 1 snapshots in index...
+
+[+] [2024-07-04 06:16:09] "www.baidu.com"
+    https://www.baidu.com/
+    > ./archive/1720073769.137125
+      > screenshot
+      > title
+        3 files (312.3 KB) in 0:00:04s
+
+[√] [2024-07-04 06:16:13] Update of 1 pages complete (4.22 sec)
+    - 0 links skipped
+    - 1 links updated
+    - 0 links had errors
+
+    Hint: To manage your archive in a Web UI, run:
+        archivebox server 0.0.0.0:8000
+"""
+
+        self.multiple_targets_success = """
+[i] [2024-07-04 06:26:40] ArchiveBox v0.7.2: archivebox add --depth=0 https://cxsecurity.com/cveshow/CVE-2022-34593/ https://github.com/Liyou-ZY/POC/issues/1 --extract title,screenshot
+    > /data
+
+[+] [2024-07-04 06:26:42] Adding 2 links to index (crawl depth=0)...
+    > Saved verbatim input to sources/1720074402-import.txt
+    > Parsed 2 URLs from input (Generic TXT)
+    > Found 2 new URLs not already in index
+
+[*] [2024-07-04 06:26:42] Writing 2 links to main index...
+    √ ./index.sqlite3
+
+[*] [2024-07-04 06:26:42] Archiving 2/7 URLs from added set...
+
+[▶] [2024-07-04 06:26:42] Starting archiving of 2 snapshots in index...
+
+[+] [2024-07-04 06:26:42] "github.com/Liyou-ZY/POC/issues/1"
+    https://github.com/Liyou-ZY/POC/issues/1
+    > ./archive/1720074402.409769
+      > screenshot
+      > title
+        3 files (347.8 KB) in 0:00:08s
+
+[+] [2024-07-04 06:26:50] "cxsecurity.com/cveshow/CVE-2022-34593"
+    https://cxsecurity.com/cveshow/CVE-2022-34593/
+    > ./archive/1720074402.409496
+      > screenshot
+      > title
+        3 files (1.2 MB) in 0:00:22s
+
+[√] [2024-07-04 06:27:13] Update of 2 pages complete (31.04 sec)
+    - 0 links skipped
+    - 2 links updated
+    - 0 links had errors
+
+    Hint: To manage your archive in a Web UI, run:
+        archivebox server 0.0.0.0:8000
+"""
+
+        self.single_target_failure = """
+[i] [2024-07-04 04:09:39] ArchiveBox v0.7.2: archivebox add --depth=1 https://asdjfhkawkisejuhdfcvhbiuewsa.com/ --extract pdf,title,screenshot
+    > /data
+
+[+] [2024-07-04 04:09:40] Adding 1 links to index (crawl depth=1)...
+    > Saved verbatim input to sources/1720066180-import.txt
+    > Parsed 1 URLs from input (Generic TXT)
+
+[*] Starting crawl of 1 sites 1 hop out from starting point
+    > Downloading https://asdjfhkawkisejuhdfcvhbiuewsa.com/ contents
+[!] Failed to download https://asdjfhkawkisejuhdfcvhbiuewsa.com/
+
+     HTTPSConnectionPool(host='asdjfhkawkisejuhdfcvhbiuewsa.com', port=443): Max retries exceeded with url: / (Caused by NameResolutionError("<urllib3.connection.HTTPSConnection object at 0x7f2e349e5110>: Failed to resolve 'asdjfhkawkisejuhdfcvhbiuewsa.com' ([Errno -2] Name or service not known)"))
+[!] Failed to get contents of URL {new_link.url} HTTPSConnectionPool(host='asdjfhkawkisejuhdfcvhbiuewsa.com', port=443): Max retries exceeded with url: / (Caused by NameResolutionError("<urllib3.connection.HTTPSConnection object at 0x7f2e349e5110>: Failed to resolve 'asdjfhkawkisejuhdfcvhbiuewsa.com' ([Errno -2] Name or service not known)"))
+    > Found 1 new URLs not already in index
+
+[*] [2024-07-04 04:09:41] Writing 1 links to main index...
+    √ ./index.sqlite3
+
+[*] [2024-07-04 04:09:41] Archiving 1/4 URLs from added set...
+
+[▶] [2024-07-04 04:09:41] Starting archiving of 1 snapshots in index...
+
+[+] [2024-07-04 04:09:41] "asdjfhkawkisejuhdfcvhbiuewsa.com"
+    https://asdjfhkawkisejuhdfcvhbiuewsa.com/
+    > ./archive/1720066180.764635
+      > pdf
+      > screenshot
+      > title
+        Extractor failed:
+            ConnectionError HTTPSConnectionPool(host='asdjfhkawkisejuhdfcvhbiuewsa.com', port=443): Max retries exceeded with url: / (Caused by NameResolutionError("<urllib3.connection.HTTPSConnection object at 0x7f2e34732cd0>: Failed to resolve 'asdjfhkawkisejuhdfcvhbiuewsa.com' ([Errno -2] Name or service not known)"))
+        Run to see full output:
+          docker run -it -v $PWD/data:/data archivebox/archivebox /bin/bash
+            cd /data/archive/1720066180.764635;
+            curl --silent --location --compressed --max-time 60 --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 ArchiveBox/0.7.2 (+https://github.com/ArchiveBox/ArchiveBox/) curl/curl 8.5.0 (x86_64-pc-linux-gnu)" "https://asdjfhkawkisejuhdfcvhbiuewsa.com/"
+
+        4 files (303.9 KB) in 0:00:01s
+
+[√] [2024-07-04 04:09:43] Update of 1 pages complete (2.02 sec)
+    - 0 links skipped
+    - 1 links updated
+    - 1 links had errors
+
+    Hint: To manage your archive in a Web UI, run:
+        archivebox server 0.0.0.0:8000
+"""
+
+        self.multiple_targets_failure = """
+[i] [2024-07-04 06:31:23] ArchiveBox v0.7.2: archivebox add --depth=0 https://sfdgcxsecurityasdfawefwAed.com/ https://afwegithubasdfasdfs.com/ --extract title,screenshot
+    > /data
+
+[+] [2024-07-04 06:31:24] Adding 2 links to index (crawl depth=0)...
+    > Saved verbatim input to sources/1720074684-import.txt
+    > Parsed 2 URLs from input (Generic TXT)
+    > Found 2 new URLs not already in index
+
+[*] [2024-07-04 06:31:24] Writing 2 links to main index...
+    √ ./index.sqlite3
+
+[*] [2024-07-04 06:31:24] Archiving 2/9 URLs from added set...
+
+[▶] [2024-07-04 06:31:24] Starting archiving of 2 snapshots in index...
+
+[+] [2024-07-04 06:31:24] "sfdgcxsecurityasdfawefwAed.com"
+    https://sfdgcxsecurityasdfawefwAed.com/
+    > ./archive/1720074684.657112
+      > screenshot
+      > title
+        Extractor failed:
+            ConnectionError HTTPSConnectionPool(host='sfdgcxsecurityasdfawefwaed.com', port=443): Max retries exceeded with url: / (Caused by NameResolutionError("<urllib3.connection.HTTPSConnection object at 0x7fae3bef1c10>: Failed to resolve 'sfdgcxsecurityasdfawefwaed.com' ([Errno -2] Name or service not known)"))
+        Run to see full output:
+          docker run -it -v $PWD/data:/data archivebox/archivebox /bin/bash
+            cd /data/archive/1720074684.657112;
+            curl --silent --location --compressed --max-time 60 --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 ArchiveBox/0.7.2 (+https://github.com/ArchiveBox/ArchiveBox/) curl/curl 8.5.0 (x86_64-pc-linux-gnu)" "https://sfdgcxsecurityasdfawefwAed.com/"
+
+        3 files (279.6 KB) in 0:00:01s
+
+[+] [2024-07-04 06:31:26] "afwegithubasdfasdfs.com"
+    https://afwegithubasdfasdfs.com/
+    > ./archive/1720074684.657393
+      > screenshot
+      > title
+        Extractor failed:
+            ConnectionError HTTPSConnectionPool(host='afwegithubasdfasdfs.com', port=443): Max retries exceeded with url: / (Caused by NameResolutionError("<urllib3.connection.HTTPSConnection object at 0x7fae3bc9c710>: Failed to resolve 'afwegithubasdfasdfs.com' ([Errno -2] Name or service not known)"))
+        Run to see full output:
+          docker run -it -v $PWD/data:/data archivebox/archivebox /bin/bash
+            cd /data/archive/1720074684.657393;
+            curl --silent --location --compressed --max-time 60 --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 ArchiveBox/0.7.2 (+https://github.com/ArchiveBox/ArchiveBox/) curl/curl 8.5.0 (x86_64-pc-linux-gnu)" "https://afwegithubasdfasdfs.com/"
+
+        3 files (273.4 KB) in 0:00:01s
+
+[√] [2024-07-04 06:31:27] Update of 2 pages complete (2.61 sec)
+    - 0 links skipped
+    - 2 links updated
+    - 2 links had errors
+
+    Hint: To manage your archive in a Web UI, run:
+        archivebox server 0.0.0.0:8000
+"""
+
+        self.mixed_targets = """
+[i] [2024-07-04 06:45:19] ArchiveBox v0.7.2: archivebox add --depth=0 https://docs.xray.cool https://asedfawecdwsac.caedws --extract title,screenshot
+    > /data
+
+[+] [2024-07-04 06:45:21] Adding 2 links to index (crawl depth=0)...
+    > Saved verbatim input to sources/1720075521-import.txt
+    > Parsed 2 URLs from input (Generic TXT)
+    > Found 2 new URLs not already in index
+
+[*] [2024-07-04 06:45:21] Writing 2 links to main index...
+    √ ./index.sqlite3
+
+[*] [2024-07-04 06:45:21] Archiving 2/11 URLs from added set...
+
+[▶] [2024-07-04 06:45:21] Starting archiving of 2 snapshots in index...
+
+[+] [2024-07-04 06:45:21] "docs.xray.cool"
+    https://docs.xray.cool
+    > ./archive/1720075521.41685
+      > screenshot
+      > title
+        3 files (540.3 KB) in 0:00:04s
+
+[+] [2024-07-04 06:45:26] "asedfawecdwsac.caedws"
+    https://asedfawecdwsac.caedws
+    > ./archive/1720075521.417146
+      > screenshot
+      > title
+        Extractor failed:
+            ConnectionError HTTPSConnectionPool(host='asedfawecdwsac.caedws', port=443): Max retries exceeded with url: / (Caused by NameResolutionError("<urllib3.connection.HTTPSConnection object at 0x7f68be631f10>: Failed to resolve 'asedfawecdwsac.caedws' ([Errno -2] Name or service not known)"))
+        Run to see full output:
+          docker run -it -v $PWD/data:/data archivebox/archivebox /bin/bash
+            cd /data/archive/1720075521.417146;
+            curl --silent --location --compressed --max-time 60 --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 ArchiveBox/0.7.2 (+https://github.com/ArchiveBox/ArchiveBox/) curl/curl 8.5.0 (x86_64-pc-linux-gnu)" "https://asedfawecdwsac.caedws"
+
+        3 files (272.7 KB) in 0:00:01s
+
+[√] [2024-07-04 06:45:27] Update of 2 pages complete (5.92 sec)
+    - 0 links skipped
+    - 2 links updated
+    - 1 links had errors
+
+    Hint: To manage your archive in a Web UI, run:
+        archivebox server 0.0.0.0:8000
+"""
+
+    def test_single_target_success(self):
+        targets = ["https://www.baidu.com/"]
+        expected_output = {
+            'links': [
+                {'status': 'Success', 'url': 'https://www.baidu.com/'}
+            ],
+            'status': 'Success'
+        }
+        result = parse_log(self.single_target_success, targets)
+        self.assertDictEqual(result, expected_output)
+
+    def test_multiple_targets_success(self):
+        targets = ["https://cxsecurity.com/cveshow/CVE-2022-34593/", "https://github.com/Liyou-ZY/POC/issues/1"]
+        expected_output = {
+            'links': [
+                {'status': 'Success', 'url': 'https://cxsecurity.com/cveshow/CVE-2022-34593/'},
+                {'status': 'Success', 'url': 'https://github.com/Liyou-ZY/POC/issues/1'}
+            ],
+            'status': 'Success'
+        }
+        result = parse_log(self.multiple_targets_success, targets)
+        self.assertDictEqual(result, expected_output)
+
+    def test_single_target_failure(self):
+        targets = ["https://asdjfhkawkisejuhdfcvhbiuewsa.com/"]
+        expected_output = {
+            'links': [
+                {'status': 'Failure', 'url': 'https://asdjfhkawkisejuhdfcvhbiuewsa.com/'}
+            ],
+            'status': 'Failure'
+        }
+        self.assertDictEqual(parse_log(self.single_target_failure, targets), expected_output)
+
+    def test_multiple_targets_failure(self):
+        targets = ["https://sfdgcxsecurityasdfawefwAed.com/", "https://afwegithubasdfasdfs.com/"]
+        expected_output = {
+            'links': [
+                {'status': 'Failure', 'url': 'https://sfdgcxsecurityasdfawefwAed.com/'},
+                {'status': 'Failure', 'url': 'https://afwegithubasdfasdfs.com/'}
+            ],
+            'status': 'Failure'
+        }
+        self.assertDictEqual(parse_log(self.multiple_targets_failure, targets), expected_output)
+
+    def test_mixed_targets(self):
+        targets = ["https://docs.xray.cool", "https://asedfawecdwsac.caedws"]
+        expected_output = {
+            'links': [
+                {'status': 'Success', 'url': 'https://docs.xray.cool'},
+                {'status': 'Failure', 'url': 'https://asedfawecdwsac.caedws'}
+            ],
+            'status': 'Partial Success'
+        }
+        self.assertDictEqual(parse_log(self.mixed_targets, targets), expected_output)
--- api/utils.py
+++ api/utils.py
@@ -3,6 +3,7 @@ import os
 import requests
 from dotenv import load_dotenv
 import re
+import yaml
 
 # 加载 .env 文件中的配置
 load_dotenv()
@@ -26,6 +27,16 @@ def error_response(message, error=None, **data):
         response["error"] = str(error)
     response.update(data)
     return response
+
+
+def execute_docker_compose_archivebox_command(command_args):
+    """执行 Docker Compose ArchiveBox 命令并处理异常"""
+    command = f"docker compose run archivebox {command_args}"
+    try:
+        subprocess.run(command, shell=True, check=True)
+        return success_response(f"Command '{command}' executed successfully.")
+    except subprocess.CalledProcessError as e:
+        return error_response(f"Failed to execute command '{command}': {e}", error=e)
 
 
 def check_docker_version():
@@ -54,8 +65,11 @@ def check_docker_compose():
         return error_response(f"Error checking Docker Compose: {e}")
 
 
-def initialize_archivebox(project_dir):
-    # 预检查 Docker 和 Docker Compose
+def initialize_archivebox():
+    """初始化ArchiveBox"""
+
+    project_dir = os.getenv('PROJECT_DIR')
+
     docker_version_result = check_docker_version()
     if docker_version_result["status"] != "success":
         return docker_version_result
@@ -88,19 +102,76 @@ def initialize_archivebox(project_dir):
     except requests.RequestException as e:
         return error_response(f"Failed to download docker-compose.yml: {e}", error=e)
 
-    # 运行初始设置创建管理员用户
-    init_command = "docker compose run archivebox init --setup"
+    # 读取并修改 docker-compose.yml 文件
+    with open(docker_compose_path, 'r') as file:
+        docker_compose = yaml.safe_load(file)
 
-    try:
-        subprocess.run(init_command, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        return error_response(f"Failed to initialize ArchiveBox: {e}", error=e)
+    admin_username = os.getenv('ADMIN_USERNAME')
+    admin_password = os.getenv('ADMIN_PASSWORD')
+    archivebox_version = os.getenv('ARCHIVEBOX_VERSION')
+
+    if admin_username and admin_password:
+        docker_compose['services']['archivebox']['environment'].append(f'ADMIN_USERNAME={admin_username}')
+        docker_compose['services']['archivebox']['environment'].append(f'ADMIN_PASSWORD={admin_password}')
+
+    if archivebox_version:
+        docker_compose['services']['archivebox']['image'] = f'archivebox/archivebox:{archivebox_version}'
+
+    with open(docker_compose_path, 'w') as file:
+        yaml.safe_dump(docker_compose, file)
+
+    # 运行初始设置创建管理员用户
+    init_result = execute_docker_compose_archivebox_command("init --setup")
+    if init_result["status"] != "success":
+        return init_result
 
     # 启动服务器
     up_command = "docker compose up -d"
-
     try:
         subprocess.run(up_command, shell=True, check=True)
         return success_response("ArchiveBox server started successfully.")
     except subprocess.CalledProcessError as e:
         return error_response(f"Failed to start ArchiveBox server: {e}", error=e)
+
+
+def remove_protocol(url):
+    url = re.sub(r'^https?://', '', url)
+    url = url.rstrip('/')
+    return url
+
+
+def extract_status(log_segment):
+    return 'Failure' if 'Extractor failed' in log_segment else 'Success'
+
+
+def parse_log(log_text, total_links):
+    stripped_links = [remove_protocol(link) for link in total_links]
+
+    result = {
+        'status': 'Success',
+        'links': []
+    }
+
+    for link in stripped_links:
+        pattern = re.compile(r'\[\+\] .*?"{}"\s+(https?://[^\s]+)'.format(re.escape(link)))
+        match = pattern.search(log_text)
+
+        if match:
+            start_pos = match.start()
+            end_pos = log_text.find('\n[', start_pos)
+            log_segment = log_text[start_pos:end_pos if end_pos != -1 else len(log_text)]
+
+            status = extract_status(log_segment)
+        else:
+            status = 'Unknown'
+
+        result['links'].append({
+            'url': total_links[stripped_links.index(link)],
+            'status': status
+        })
+
+    if any(link['status'] == 'Failure' for link in result['links']):
+        result['status'] = 'Partial Success' if any(
+            link['status'] == 'Success' for link in result['links']) else 'Failure'
+
+    return result
